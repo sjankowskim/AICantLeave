faf5fb65fecfaea2b3d457d0822a42bbd2e46396
sjankowskim <sebastian@jankowski.com>
2024-05-09 17:07:42 -0400

    Upload full project

---
+++ AICantLeave/AICantLeave.cs
@@ -0,0 +1,19 @@
+﻿using ThunderRoad;
+
+namespace AICantLeave
+{
+    public class AICantLeave : ThunderScript
+    {
+        public override void ScriptLoaded(ModManager.ModData modData)
+        {
+            base.ScriptLoaded(modData);
+            EventManager.OnPlayerSpawner += OnPlayerSpawner;
+
+        }
+
+        private void OnPlayerSpawner(PlayerSpawner playerSpawner, EventTime eventTime)
+        {
+            if (eventTime == EventTime.OnEnd) FleePoint.list.Clear();
+        }
+    }
+}
