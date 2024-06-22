41c0954946cc25e05b83651fa464ed547bab55d7
sjankowskim <sebastian@jankowski.com>
2024-06-22 13:50:37 -0400

    Update to 1.0. Add ability to enable/disable mod.

--- AICantLeave/AICantLeave.cs
+++ AICantLeave/AICantLeave.cs
@@ -1,19 +1,48 @@
-﻿using ThunderRoad;
+﻿using System.Collections.Generic;
+using ThunderRoad;
 
 namespace AICantLeave
 {
     public class AICantLeave : ThunderScript
     {
+        private static List<FleePoint> currentFleePoints;
+        private static bool isModEnabled;
+
+        [ModOption(name: "Use AICantLeave Mod", tooltip: "Determines whether to use the AICantLeave mod.", defaultValueIndex = 1)]
+        public static void UseMod(bool useMod)
+        {
+            isModEnabled = useMod;
+
+            if (useMod)
+            {
+                FleePoint.list.Clear();
+            } 
+            else
+            {
+                FleePoint.list.AddRange(currentFleePoints);
+            }
+        }
+
         public override void ScriptLoaded(ModManager.ModData modData)
         {
             base.ScriptLoaded(modData);
-            EventManager.OnPlayerSpawner += OnPlayerSpawner;
+            EventManager.onLevelLoad += OnLevelLoad;
 
         }
 
-        private void OnPlayerSpawner(PlayerSpawner playerSpawner, EventTime eventTime)
+        private void OnLevelLoad(LevelData levelData, LevelData.Mode mode, EventTime eventTime)
         {
-            if (eventTime == EventTime.OnEnd) FleePoint.list.Clear();
+            if (eventTime == EventTime.OnStart)
+            {
+                return;
+            }
+
+            currentFleePoints = new List<FleePoint>(FleePoint.list);
+
+            if (isModEnabled)
+            {
+                FleePoint.list.Clear();
+            }
         }
     }
 }
